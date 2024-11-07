47dc9e6e444fb7a6b7cb2235b57dfd7ad21f0b7c
Benoit Koenig <koenigbenoit47@gmail.com>
2024-11-07 12:20:54 +0100

    Move src files to 'src/'

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
 
 export default {
-  input: "./index.ts",
+  input: "./src/index.ts",
   output: {
     dir: "dist",
   },
