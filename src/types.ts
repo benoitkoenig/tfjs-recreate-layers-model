16c2492e42d1d92d117201503437ab8456ca557e
Benoit Koenig <koenigbenoit47@gmail.com>
2024-11-09 15:36:27 +0100

    Fix indentation in types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,7 @@
 import type { layers } from "@tensorflow/tfjs-layers";
 
 export interface LayerRecreationData {
-    originalLayer: layers.Layer;
-    recreatedLayer: layers.Layer;
-    requiresWeightsReset: boolean;
-  }
-  
+  originalLayer: layers.Layer;
+  recreatedLayer: layers.Layer;
+  requiresWeightsReset: boolean;
+}
